46bf9a1857eff95636ce05a885a33b74f638077a
tiagojosenunes <tiagojose580@gmail.com>
2026-09-19 18:51:36 -0300

    refactor(ui): adiciona suporte a k6/browser no cenario do navegador

--- scenarios/browser-script.js
+++ scenarios/browser-script.js
@@ -1,51 +1,37 @@
-import http from "k6/http";
-import exec from 'k6/execution';
-import { browser } from "k6/browser";
+import { browser } from 'k6/browser';
 import { sleep, fail } from 'k6';
-import { expect } from "https://jslib.k6.io/k6-testing/0.5.0/index.js";
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js'
+import { expect } from 'https://jslib.k6.io/k6-testing/0.5.0/index.js';
 
-export function handleSummary(data) {
-  return {
-    'summary.html': htmlReport(data),
-  }
-}
+const BASE_URL = __ENV.BASE_URL || 'https://quickpizza.grafana.com';
 
-const BASE_URL = __ENV.BASE_URL || "https://quickpizza.grafana.com";
-
+// Configuração necessária para habilitar o k6/browser na execução isolada
 export const options = {
   scenarios: {
     ui: {
-      executor: "shared-iterations",
+      executor: 'shared-iterations',
       vus: 1,
       iterations: 1,
       options: {
         browser: {
-          type: "chromium",
+          type: 'chromium',
         },
       },
     },
   },
 };
 
-export function setup() {
-  let res = http.get(BASE_URL);
-  expect(res.status, `Got unexpected status code ${res.status} when trying to setup. Exiting.`).toBe(200);
-}
-
-export default async function() {
-  let checkData;
+export default async function UIQuickPizza() {
   const page = await browser.newPage();
 
   try {
     await page.goto(BASE_URL);
-    await expect.soft(page.locator("h1")).toHaveText("Looking to break out of your pizza routine?");
+    await expect.soft(page.locator('h1')).toHaveText('Looking to break out of your pizza routine?');
 
     await page.locator('//button[. = "Pizza, Please!"]').click();
     await page.waitForTimeout(500);
 
-    await page.screenshot({ path: "screenshot.png" });
-    await expect.soft(page.locator("div#recommendations")).not.toHaveText("");
+    await page.screenshot({ path: 'screenshot.png' });
+    await expect.soft(page.locator('div#recommendations')).not.toHaveText('');
   } catch (error) {
     fail(`Browser iteration failed: ${error.message}`);
   } finally {
@@ -54,4 +40,3 @@ export default async function() {
 
   sleep(1);
 }
-
