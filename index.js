264a553e87836440fbefb391d82ed963e9410aa3
tiagojosenunes <tiagojose580@gmail.com>
2026-09-19 13:58:53 -0300

    feat: inserindo browser script

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import GetCustomer from './scenarios/Get-Customer.js';
 import PostLogin from './scenarios/Post-Login.js';
 import { group, sleep } from 'k6';
+import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js'
+
+export function handleSummary(data) {
+  return {
+    'summary.html': htmlReport(data),
+  }
+}
 
 export default () => {
     group('Endpoint Post Login - Autenticação', () => {
