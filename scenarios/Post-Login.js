4b459d5cc75f4b643ed0e38290910404b0ec2851
tiagojosenunes <tiagojose580@gmail.com>
2026-09-19 18:44:27 -0300

    refactor: ajusta SLAs

--- scenarios/Post-Login.js
+++ scenarios/Post-Login.js
@@ -2,15 +2,14 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend, Rate, Counter } from 'k6/metrics';
 
-// Métricas personalizadas para o Login
+// Métricas personalizadas
 export let LoginDuration = new Trend('login_duration');
 export let LoginFailRate = new Rate('login_fail_rate');
-export let LoginSuccessRate = new Rate('login_success_rate');
 export let LoginReqs = new Counter('login_reqs');
 
 export default function PostLogin() {
   const url = 'https://quickpizza.grafana.com/api/users/token';
-  
+
   const payload = JSON.stringify({
     username: 'admin',
     password: 'password',
@@ -22,17 +21,16 @@ export default function PostLogin() {
     },
   };
 
-  let res = http.post(url, payload, params);
+  const res = http.post(url, payload, params);
 
-  // Registro das métricas
+  // Registro de métricas
   LoginDuration.add(res.timings.duration);
   LoginReqs.add(1);
-  LoginFailRate.add(res.status === 0 || res.status >= 400);
-  LoginSuccessRate.add(res.status < 400);
+  LoginFailRate.add(res.status !== 200);
 
   check(res, {
     'status é 200': (r) => r.status === 200,
-    'token retornado': (r) => r.json('token') !== undefined,
+    'recebeu token JWT': (r) => r.json('token') !== undefined,
   });
 
   sleep(1);
