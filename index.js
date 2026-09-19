3f8beb3a2dd55ebf94dcd88afaea022b59ad601c
tiagojosenunes <tiagojose580@gmail.com>
2026-09-19 19:45:04 -0300

    chore: configura k6 para exportar métricas para o Grafana Cloud

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ import PostLogin from './scenarios/Post-Login.js';
 import UIQuickPizza from './scenarios/browser-script.js';
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
 
-// Leitura dinâmica das configurações externas
 const settings = JSON.parse(open('./settings.json'));
 
 export const options = {
+  // A propriedade cloud foi mantida apenas com o nome, omitindo o projectID
+  cloud: {
+    name: 'QuickPizza Load Test Suite',
+  },
   thresholds: settings.thresholds,
   scenarios: {
-    // Cenário 1: Carga concorrente na API de Login
     api_login_load: {
       executor: 'ramping-vus',
       startVUs: 0,
       stages: settings.stages,
       exec: 'runApiLogin',
     },
-    // Cenário 2: Validação visual e renderização na UI
     ui_browser_test: {
       executor: 'shared-iterations',
       vus: 1,
@@ -30,7 +31,6 @@ export const options = {
   },
 };
 
-// Funções de execução dos cenários
 export function runApiLogin() {
   PostLogin();
 }
@@ -39,7 +39,6 @@ export async function runUiTest() {
   await UIQuickPizza();
 }
 
-// Geração automática do relatório HTML
 export function handleSummary(data) {
   return {
     'summary.html': htmlReport(data),
